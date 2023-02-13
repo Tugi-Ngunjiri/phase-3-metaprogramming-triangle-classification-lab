d6de94e62ada77a6514f2f68e9c0b78faabcacaa
tugi <blueqwinn@gmail.com>
2023-02-13 15:52:52 +0300

    first commit

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,29 @@
 class Triangle
   # write code here
+  # triangle code
+ 
+  attr_reader :equilateral, :isosceles, :scalene, :a , :b , :c
+   def  initialize(a, b , c)
+    @a = a
+    @b =b
+    @c = c
+   end
+
+  def kind
+  
+    if a== b && b == c
+      :equilateral
+    elsif a !=b  && b  !=c && c !=a
+      :scalene
+    else
+      :isosceles
+    end
+
+  end
+
+
+class TriangleError< StandardError
+  # triangle error code
+
+end
 end
